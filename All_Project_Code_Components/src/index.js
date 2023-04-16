e0099c0a326024313ce6df738edde03e97f68743
Andrey Lototskiy <andrey.lototskiy@gmail.com>
2023-04-16 14:52:13 -0600

    Added basic test suite functionality

--- All_Project_Code_Components/src/index.js
+++ All_Project_Code_Components/src/index.js
@@ -6,11 +6,17 @@ const express = require('express'); // To build an application server or API
 const app = express();
 const pgp = require('pg-promise')(); // To connect to the Postgres DB from the node server
 
+// *****************************************************
 
+app.get('/welcome', (req, res) => {
+    res.json({status: 'success', message: 'Welcome!'});
+  });
+
+// *****************************************************
 
 // *****************************************************
 // <!-- Section 5 : Start Server-->
 // *****************************************************
 // starting the server and keeping the connection open to listen for more requests
-app.listen(3000);
+module.exports = app.listen(3000);
 console.log('Server is listening on port 3000');
