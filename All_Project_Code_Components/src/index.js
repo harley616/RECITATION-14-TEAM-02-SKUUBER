82fc094dfd1f3f818cb4d0710241af5ebb37b44e
Andrey Lototskiy <andrey.lototskiy@gmail.com>
2023-04-16 14:10:51 -0600

    Completed Docker-compose setup

---
+++ All_Project_Code_Components/src/index.js
@@ -0,0 +1,16 @@
+// *****************************************************
+// <!-- Section 1 : Import Dependencies -->
+// *****************************************************
+
+const express = require('express'); // To build an application server or API
+const app = express();
+const pgp = require('pg-promise')(); // To connect to the Postgres DB from the node server
+
+
+
+// *****************************************************
+// <!-- Section 5 : Start Server-->
+// *****************************************************
+// starting the server and keeping the connection open to listen for more requests
+app.listen(3000);
+console.log('Server is listening on port 3000');
