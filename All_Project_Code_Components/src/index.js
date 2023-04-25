b9c0ab5bd0624b3dff5ada018d52e1420298b85f
Andrey Lototskiy <andrey.lototskiy@gmail.com>
2023-04-25 12:53:43 -0600

    Simplified Friend API

--- All_Project_Code_Components/src/index.js
+++ All_Project_Code_Components/src/index.js
@@ -26,6 +26,8 @@ function Event(event_id, owner, name, date, location) {
     this.location = location;
 }
 
+let sessionUser;
+
 // *****************************************************
 // <!-- Section 2 : Connect to DB -->
 // *****************************************************
@@ -246,7 +248,7 @@ app.post('/register', async (req, res) => {
 //--------------------------------------------- L O G I N ---------------------------------------------------------//
 
 app.get('/login', (req, res) => {
-   res.render('pages/login')
+   res.render('pages/login', {loginFailed: false})
 });
 
 // Login
@@ -255,11 +257,12 @@ app.post('/login', async (req, res) => {
   console.log('password: ', req.body.password);
   //console.log('hashed password: ',  await bcrypt.hash(req.body.password, 10));
   const username= req.body.username;
+  sessionUser = username;
   try {
   const user = await db.query('SELECT * FROM users WHERE username = $1', [username]);  //Checking if username exists in the table
   if(user.length === 0){                                                                        //if username does not exist: 
     console.log('Username not found.');
-    await res.redirect('/register');                                                  //redirect to registration page
+    await res.render('pages/login', {loginFailed: true});                                                  //redirect to registration page
   } else {                                                                                      //if username does exist:
     console.log('Username found. Matching passwords...');
     console.log('Inputted password: ', req.body.password);
@@ -285,268 +288,55 @@ app.post('/login', async (req, res) => {
   }
 });
 
-app.post("/addFriendRequest", (req, res) => {
-    const query = "INSERT INTO friend_add_queue (username, friend_username) VALUES ($1, $2)";
-    const values = [req.session.user[0]['username'], req.body.friend_username];
-    db.any(query, values)
-      .then(function () {
-        console.log("Successfully added friend request!");        
-        console.log('from ' + req.session.user[0]['username'])
-        console.log('friend request to ' + req.body.friend_username);
-        console.log(req.body)
-        console.log(values)
-        res.redirect("/home");
-      })
-      .catch(function (err) {
-        console.log(err);
-        res.redirect("/home");
-      });
-  });
 
-app.get("/acceptFriend", (req, res) => {
-    console.log('accepting friend : ' + req.query.username + ' as ' + req.session.user[0]['username'])
-    const friend_username = req.session.user[0]['username'];
-    const username = req.query.username
-    console.log('deleting add_friend_queue')
-    // because this is from the perspective of the other person, we swap $2 and $1
-    const query = "DELETE FROM friend_add_queue WHERE username = $1 AND friend_username = $2;";
-    const values = [username, friend_username];
-    db.any(query, values)
-      .then(function () {
-        console.log('successfully deleted.')
-        console.log('adding relationship into user_friends.')
-        const query1 = "INSERT INTO user_friends (username, friend_username) VALUES ($1, $2)";
-        const query2 = "INSERT INTO user_friends (username, friend_username) VALUES ($2, $1)";
-        //const values = [req.session.user[0]['username'], req.query.friend_username];
-        db.task('get-everything', task => {
-          return task.batch([task.any(query1,values), task.any(query2,values)]);
-        })
-          .then(function () {
-            console.log("Successfully added friend: friend " + values[1] + " for " + values[0]);
-            res.redirect("/home");
-          })
-          .catch(function (err) {
-            console.log(err);
-            res.redirect("/home");
-          });
-      })
-      .catch(function (err) {
-        console.log(err);
-        res.redirect("/home");
+
+  // ------------------------------------------------------------------
+
+ 
+
+  app.get('/home', (req, res) => {
+    const user = sessionUser;
+    const query = "SELECT * FROM users where users.username != $1";
+
+    db.any(query, [user])
+    .then(users => {
+      res.render('pages/home', {
+        users
       });
-});
-  app.delete("/declineFriendRequest", (req, res) => {
-    const query = "DELETE FROM friend_add_queue WHERE username = $1 AND friend_username = $2;";
-    const values = [req.session.user, req.body.friend_username];
-    db.any(query, values)
-      .then(function () {
-        console.log("Successfully deleted friend request from queue!");
-        res.redirect("/home");
-      })
-      .catch(function (err) {
-        console.log(err);
-        res.sendStatus(500);
-      });
-  });
-  app.get('/friendList', function (req, res) {
-    // Fetch query parameters from the request object
-    var username = req.session.user;
-    // Multiple queries using templated strings
-    var friend_request = `select * from user_friends where username=$1`;
-    // use task to execute multiple queries
-    db.task('get-everything', task => {
-      return task.batch([task.any(friend_request, [username])]);
     })
-      // if query execution succeeds
-      // query results can be obtained
-      // as shown below
-      .then(data => {
-        res.status(200).json({
-          Friends: data[0],
-  
-        });
-      })
-      // if query execution fails
-      // send error message
-      .catch(err => {
-        console.log('Uh Oh spaghettio');
-        console.log(err);
-        res.status('400').json({
-          current_user: '',
-          city_users: '',
-          error: err,
-        });
-      });
-  });
-  
-
-app.get('/friendRequestList', function (req, res) {
-    
-    // Fetch query parameters from the request object
-    var username = req.session.user[0]['username'];
-
-    console.log('friend request list')
-    console.log(username)
-    // Multiple queries using templated strings
-    var friend_add_queue = `select * from friend_add_queue`;
-
-    console.log(friend_add_queue)
-    // use task to execute multiple queries
-    db.any(friend_add_queue)
-      // if query execution succeeds
-      // query results can be obtained
-      // as shown below
-      .then(data => {
-
-        console.log("the friend request list for")
-        console.log(username)
-        console.log(data)
-        res.status(200).json({
-          FriendReq: data[0],
-        });
-      })
-      // if query execution fails
-      // send error message
-      .catch(err => {
-        console.log('Uh Oh spaghettio');
-        console.log(err);
-        res.status('400').json({
-          current_user: '',
-          city_users: '',
-          error: err,
-        });
-      });
-  });
-
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-}
-
-app.get('/home', async (req, res) => {
-    // Fetch query parameters from the request object
-    var username = req.session.user[0]['username'];
-    // Multiple queries using templated strings
-    var friend_add_queue = `select * from friend_add_queue where friend_username = $1`;
-    // use task to execute multiple queries
-    const friend_req_data = await db.any(friend_add_queue, username)
-    console.log("the friend request list for: " + username)
-    console.log(friend_req_data)
-
-    var friend_list_query = `select * from user_friends where username = $1`;
-    const friend_data = await db.any(friend_list_query, username)
-    console.log("the friend list for: " + username) 
-    console.log(friend_data)
-    res.render('pages/home', {user: req.session.user[0]['username'],
-    friend_request_list: friend_req_data,
-    friend_list: friend_data
+    .catch((err) => {
+      console.log('Error: users api failed.');
+      console.log(err);
+      res.render('pages/home'), {
+        users: []
+      };
     })
-});
+  });
 
-app.get('/friendTest', (req, res) => {
-    console.log('user')
-    console.log(req.session.user)
+  app.post('/home/add_friends', (req, res) => {
+    const query = " INSERT INTO user_friends(username, friend_username) VALUES ($1, $2);";
+    console.log(sessionUser);
+    console.log(req.body.username);
+    db.any(query, [sessionUser, req.body.username])
+    .then((users) => {
+      res.render("pages/home", {
+        users
+      });
+    })
+    .catch((err) => {
+      console.log(err),
+      res.render("pages/home", {
+        users: [],
+        error: true,
+      })
+    });
+  });
 
-});
+  // ------------------------------------------------------------------
 
 // add an event for a user
 // must have a NAME, TIME (in hours, 0-24), LOCATION (Boulder, CO)
-app.post('/addEvent', async (req, res) => {
-    const username = req.session.user[0]['username'];
-    const title = req.body.title;
-    const date = req.body.date;
-    const time = req.body.time;
-    const location = req.body.location;
-    const full_date = date + 'T' + time;
 
-    // add the event to events table
-    const add_event_query = "INSERT INTO events (owner, date, title, location) VALUES ($1, $2, $3, $4)";
-    var values = [username, full_date, title, location];
-    const add_event_result = await db.any(add_event_query, values);
-
-    // get the id of the event
-    const event_id_query = "SELECT event_id FROM events WHERE owner = $1 AND date = $2 AND title = $3 AND location = $4";
-    var values = [username, full_date, title, location];
-    const event_id_result = await db.any(event_id_query, values);
-
-    console.log('event id result')
-    const event_id = event_id_result[0]['event_id'];
-    console.log(event_id)
-
-    const u2e_query = "INSERT INTO users_to_events (username, event_id) VALUES ($1, $2)";
-    var values = [username, event_id];
-    const u2e_result = await db.any(u2e_query, values);
-    console.log('SUCC!!!!!!!1!!!!!');
-    res.render('pages/calendar');
-})
-
-// get list of events for a user
-// Event{date: '2020-04-20T08:00', event_id: 1, owner: 'a', title: 'test', location: 'Boulder, CO'}
-
-app.get('/getFriendEvents', async (req, res) => {
-    const owner = req.query.friend_username; //username of the person whose events to get
-    const get_events_query = `select * from events where owner = $1`;
-    var values  = [friend_username];
-    const get_event_result = await db.any(get_events_query, values);
-    console.log('successfully got events for user: ' + req.session.user[0]['username']);
-    return res.status(200).json(get_event_result);
-})
-
-app.post('/addSelfToFriendEvent', async (req, res) => {
-    const username = req.session.user[0]['username'];
-    const event_id = req.body.event_id; // req.query.event_id id of the event to add self to 
-    const set_users_to_events = `insert INTO users_to_events (username, event_id) VALUES ($1, $2)`;
-    var values  = [username, event_id];
-    const get_event_result = await db.any(set_users_to_events, values);
-    console.log('successfully added: ' + username + " to event id: " + event_id);
-    console.log(get_event_result);
-    return res.redirect('/calendar');
-})
-
-app.get('/getAddedEvents', async (req, res) => {
-    // get all event_id from users_to_events where youre the user
-    const username = req.session.user[0]['username'];
-    const u2e_query = `select * from users_to_events where username = $1`;
-    var values  = [username];
-    const u2e_result = await db.any(u2e_query, values);
-    //get a list of the event ids
-    const mapped_u2e_result = u2e_result.map((u2e) => u2e['event_id']);
-    // list of Event object
-    const full_events_list = [];
-    for (let i = 0; i < mapped_u2e_result.length; i++) {
-        const current_event_id = mapped_u2e_result[i];
-        const get_event_query = `select * from events where event_id = $1`;
-        var values  = [current_event_id];
-        const get_event_result = await db.any(get_event_query, values);
-        full_events_list.push(get_event_result[0]);
-    }
-    return res.status(200).json(full_events_list)
-})
-
-// get the events for a user
-app.get('/getEvents', async (req, res) => {
-    const owner = req.session.user[0]['username'];
-    const get_events_query = `select * from events where owner = $1`;
-    var values  = [owner];
-    const get_event_result = await db.any(get_events_query, values);
-    console.log('successfully got events for user: ' + owner);
-    return res.status(200).json(get_event_result);
-})
-
-
-//same as acceptFriend, except we don't add the relationship into user_friends
-app.get("/declineFriend", async (req, res) => {
-    console.log('accepting friend : ' + req.query.username + ' as ' + req.session.user[0]['username'])
-    const friend_username = req.session.user[0]['username'];
-    const username = req.query.username
-    console.log('deleting add_friend_queue')
-    // because this is from the perspective of the other person, we swap $2 and $1
-    const query = "DELETE FROM friend_add_queue WHERE username = $1 AND friend_username = $2;";
-    const values = [username, friend_username];
-    const query_result = await db.any(query, values);
-    console.log('successfully deleted.')
-    console.log(query_result);
-    res.redirect("/home");
-});
 
 app.get("/logout", (req, res) => {
     req.session.destroy();
--- All_Project_Code_Components/src/init_data/create.sql
+++ All_Project_Code_Components/src/init_data/create.sql
@@ -3,22 +3,14 @@ CREATE TABLE users (
     username VARCHAR(50) PRIMARY KEY,
     password CHAR(60) NOT NULL
 );
+
 DROP TABLE IF EXISTS user_friends;
 CREATE TABLE user_friends (
     username VARCHAR(50) NOT NULL,
     friend_username VARCHAR(50) NOT NULL,
-    PRIMARY KEY (username, friend_username),
-    FOREIGN KEY (username) REFERENCES users(username) ON DELETE CASCADE,
-    FOREIGN KEY (friend_username) REFERENCES users(username) ON DELETE CASCADE
 );
 DROP TABLE IF EXISTS friend_add_queue;
-CREATE TABLE friend_add_queue (
-    username VARCHAR(50) NOT NULL,
-    friend_username VARCHAR(50) NOT NULL,
-    PRIMARY KEY (username, friend_username),
-    FOREIGN KEY (username) REFERENCES users(username),
-    FOREIGN KEY (friend_username) REFERENCES users(username)
-);
+
 
 DROP TABLE IF EXISTS events;
 CREATE TABLE events (
