57cc08e15b7b7e035bf4f48d704e37b815dcd5b0
Harley Ewert <harley.ewert@colorado.edu>
2023-04-26 08:28:25 -0600

    still trying to get this branch up to date

--- All_Project_Code_Components/src/index.js
+++ All_Project_Code_Components/src/index.js
@@ -501,7 +501,10 @@ app.post("/MyCalendar", async (req, res) => {
         calendar: calendar(year),
         months,
         year
+<<<<<<< HEAD
 
+=======
+>>>>>>> 09964e3 (works but calendar only shows january)
       })
     })
     .catch(error => {
