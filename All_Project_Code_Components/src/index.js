6adfb7f0b77262fd7cfbf990c8272607cecf92ae
Sarah Bian <sabi3820@colorado.edu>
2023-05-01 18:54:39 -0600

    Start Page

--- All_Project_Code_Components/src/index.js
+++ All_Project_Code_Components/src/index.js
@@ -87,7 +87,7 @@ app.use(express.static("public"));
 // TODO - Include your API routes here
 
 app.get('/', (req, res) => {
-  res.render('pages/login')
+  res.render('pages/startpage')
 });
 
 app.get('/welcome', (req, res) => {
